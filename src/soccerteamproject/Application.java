5cc62c7fb7486ee8d5a07dbf25d28814de5a5974
macarious <63441014+macarious@users.noreply.github.com>
2023-04-15 12:00:26 -0700

    Add 4 initial frames to view.

--- src/soccerteamproject/Application.java
+++ src/soccerteamproject/Application.java
@@ -1,7 +1,12 @@
 package soccerteamproject;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 
 /**
  * This is a implementation of {@link ApplicationInterface} which uses a graphical user interface.
@@ -11,16 +16,59 @@ import javax.swing.JFrame;
  */
 public class Application extends JFrame implements ApplicationInterface {
 
+  private final JPanel panel1; // User input players information and contains buttons which create teams.
+  private final JPanel panel2; // Display all registered players.
+  private final JPanel panel3; // Display the list of team players after team has been created.
+  private final JPanel panel4; // Display the starting line up after team has been created.
+  private final Color colourBG;
+  private final Color colourBorder;
+
   public Application() {
-    this.setTitle("Soccer Team Application");
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setLayout(new FlowLayout());
-    this.setSize(800, 400);
-    this.setLocation(200, 200);
+
+    // Default colours.
+    colourBG = Color.DARK_GRAY;
+    colourBorder = Color.LIGHT_GRAY;
+
+    // Set up main application frame.
+    setTitle("Soccer Team Application");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLayout(new FlowLayout());
+    setLocation(200, 200);
+    getContentPane().setBackground(colourBG);
+
+    // Create three panels side-by-side.
+    panel1 = new JPanel();
+    panel2 = new JPanel();
+    panel3 = new JPanel();
+    panel4 = new JPanel();
+    add(panel1);
+    add(panel2);
+    add(panel3);
+    add(panel4);
+
+    // Customize Panel 1.
+    panel1.setPreferredSize(new Dimension(350, 600));
+    panel1.setBorder(new LineBorder(colourBorder, 2));
+    panel1.setBackground(colourBG);
+
+    // Customize Panel 2.
+    panel2.setPreferredSize(new Dimension(350, 600));
+    panel2.setBorder(new LineBorder(colourBorder, 2));
+    panel2.setBackground(colourBG);
+
+    // Customize Panel 3.
+    panel3.setPreferredSize(new Dimension(350, 600));
+    panel3.setBorder(new LineBorder(colourBorder, 2));
+    panel3.setBackground(colourBG);
+
+    // Customize Panel 4.
+    panel4.setPreferredSize(new Dimension(350, 600));
+    panel4.setBorder(new LineBorder(colourBorder, 2));
+    panel4.setBackground(colourBG);
+
 
     pack();
     setVisible(true);
-
   }
 
   @Override
